20df7db3572599ae752d9d80066e48cb0a4bd568
Godfrey Chan <godfreykfc@gmail.com>
2023-10-31 14:44:13 -0700

    fix ember-try + add 3.28

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -5,7 +5,25 @@ const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
 
 module.exports = async function () {
   return {
+    useYarn: true,
     scenarios: [
+      {
+        name: 'ember-lts-3.28',
+        npm: {
+          devDependencies: {
+            // sketchy town!
+            // ember-source has a check for this.project.bowerDependencies()
+            // which was removed in ember-cli 5. not sure if swapping out
+            // ember-cli with ember-try (which is itself running on the
+            // currently installed ember-cli version) is really ok, but it
+            // seems to work?
+            'ember-cli': '~4.12.0',
+            'ember-source': '~3.28.0',
+            'ember-qunit': '~6.2.0',
+            '@ember/test-helpers': '~2.9.4',
+          },
+        },
+      },
       {
         name: 'ember-lts-4.8',
         npm: {
